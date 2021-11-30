406daa305937ce9eff75b6e6a9aa26a3e5ce4103
Johanna Simonsson <johanna.simonsson@klarna.com>
2021-11-30 14:57:39 +0100

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,31 @@ use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::{event, terminal, ExecutableCommand};
-use rust_invaders::frame::new_frame;
 use rust_invaders::frame::Drawable;
+use rust_invaders::frame::{new_frame, Frame};
 use rust_invaders::invaders::Invaders;
 use rust_invaders::player::Player;
 use rust_invaders::{frame, render};
 use rusty_audio::Audio;
 use std::error::Error;
+use std::io::Stdout;
 use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread::JoinHandle;
 use std::time::{Duration, Instant};
 use std::{io, thread};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = initialize_audio();
+    let mut stdout = initialize_terminal()?;
+
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || render(render_rx));
+
+    game_loop(&mut audio, &render_tx)?;
+    cleanup(&mut audio, &mut stdout, render_tx, render_handle)?;
+    Ok(())
+}
 
 fn initialize_audio() -> Audio {
     let sounds: Vec<&str> = vec!["explode", "lose", "move", "pew", "startup", "win"];
@@ -20,39 +35,37 @@ fn initialize_audio() -> Audio {
     for sound in sounds {
         audio.add(sound, &format!("{}.wav", sound.to_string()))
     }
+    audio.play("startup");
     audio
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut audio = initialize_audio();
-    audio.play("startup");
-
-    // Terminal
+fn initialize_terminal() -> Result<Stdout, Box<dyn Error>> {
     let mut stdout = io::stdout();
     terminal::enable_raw_mode()?;
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide);
+    Ok(stdout)
+}
 
-    // Render Loop in a separate thread
-    let (render_tx, render_rx) = mpsc::channel();
-    let render_handle = thread::spawn(move || {
-        let mut last_frame = frame::new_frame();
-        let mut stdout = io::stdout();
-        render::render(&mut stdout, &last_frame, &last_frame, true);
-        loop {
-            let curr_frame = match render_rx.recv() {
-                Ok(x) => x,
-                Err(_) => break,
-            };
-            render::render(&mut stdout, &last_frame, &curr_frame, false);
-            last_frame = curr_frame;
-        }
-    });
+fn render(render_rx: Receiver<Frame>) {
+    let mut last_frame = frame::new_frame();
+    let mut stdout = io::stdout();
+    render::render(&mut stdout, &last_frame, &last_frame, true);
+    loop {
+        let curr_frame = match render_rx.recv() {
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        render::render(&mut stdout, &last_frame, &curr_frame, false);
+        last_frame = curr_frame;
+    }
+}
 
-    // Game Loop
+fn game_loop(audio: &mut Audio, render_tx: &Sender<Frame>) -> Result<(), Box<dyn Error>> {
     let mut player = Player::new();
     let mut instant = Instant::now();
     let mut invaders = Invaders::new();
+
     'gameloop: loop {
         //Per-frame init
         let delta = instant.elapsed();
@@ -105,8 +118,15 @@ fn main() -> Result<(), Box<dyn Error>> {
             break 'gameloop;
         }
     }
+    Ok(())
+}
 
-    // Cleanup
+fn cleanup(
+    audio: &mut Audio,
+    stdout: &mut Stdout,
+    render_tx: Sender<Frame>,
+    render_handle: JoinHandle<()>,
+) ->Result<(),Box<dyn Error>> {
     drop(render_tx);
     render_handle.join().unwrap();
     audio.wait();
